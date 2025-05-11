29ab829cdd4981de37137efaafc8997c6066752c
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-05-10 23:29:02 -0400

    upload basic template

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-grow flex-col items-center justify-center p-6 text-3xl font-bold text-gray-900">
+      <h1>Hello Next with Tailwind 3 !</h1>
+    </div>
+  );
+}
