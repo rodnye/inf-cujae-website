f26bd3e62b2cac144301864583030fe61344780e
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-05-13 18:59:11 -0400

    feat: create SlideBar dialog

---
+++ src/components/dialogs/SlideBar.tsx
@@ -0,0 +1,24 @@
+import { AnimatePresence, motion } from 'framer-motion';
+
+interface Props {
+  children: React.ReactNode;
+  open?: boolean;
+}
+
+export const SlideBar: React.FC<Props> = ({ children, open }) => {
+  return (
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          initial={{ left: '-100%' }}
+          animate={{ left: 0 }}
+          exit={{ left: '-100%' }}
+          transition={{ duration: 0.6, delay: 0, ease: 'easeInOut' }}
+          className="absolute top-0 h-full"
+        >
+          <div className="h-full w-full flex-col">{children}</div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
