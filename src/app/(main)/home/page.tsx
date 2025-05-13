75413136d8f8c0a169e14ff9cdf1436afeb3a311
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-05-13 19:00:17 -0400

    feat: create basic /home page

---
+++ src/app/(main)/home/page.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from '@/components/sections/Navbar';
+
+export default function HomePage() {
+  return (
+    <div className="overflox-x-hidden flex w-full flex-grow flex-col text-white">
+      <Navbar />
+
+      {
+        // TODO: seccion hero
+      }
+      <section></section>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: '#dedede',
+        primary: '#222',
+      },
+    },
   },
   plugins: [],
 };
