8f618643a735b8a876b89e3c7322eb338ee6f02e
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-05-13 18:57:37 -0400

    feat: add redirect on root dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <div className="flex flex-grow flex-col items-center justify-center p-6 text-3xl font-bold text-gray-900">
-      <h1>Hello Next with Tailwind 3 !</h1>
-    </div>
-  );
+import { redirect } from 'next/navigation';
+
+export default function HomePage() {
+  redirect('/home');
 }
