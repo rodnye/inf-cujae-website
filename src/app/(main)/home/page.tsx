a6b6a1cffa93cdd559864de564e97c12f39c5154
herbertDev12 <herbertn740@gmail.com>
2025-05-16 23:43:39 -0500

    feat:Add footer component structure

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/sections/Hero';
 import { Navbar } from '@/components/sections/Navbar';
+import Footer from '@/components/sections/Footer';
 
 export default function HomePage() {
   return (
     <div className="overflox-x-hidden flex w-full flex-grow flex-col text-white">
       <Navbar />
       <Hero />
+      <Footer />
       <section></section>
     </div>
   );
---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-opacity-95p-4 mt-auto w-full bg-primary sm:p-6">
+      <div className="mx-auto w-full p-4 md:max-w-screen-2xl">
+        <div className="md:flex md:justify-between">
+          <div className="flex w-full flex-col justify-between gap-8 text-white sm:gap-6 md:flex-row md:flex-wrap md:gap-10">
+            <div className="store-data-logo flex flex-col gap-8 text-sm"></div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
