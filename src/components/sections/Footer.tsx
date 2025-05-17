f364324c28c86673cf94715da84fe23264fdd927
herbertDev12 <herbertn740@gmail.com>
2025-05-17 00:37:00 -0500

    feat:Add a logo_infor.png to the Footer component

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,10 +1,25 @@
+import Image from 'next/image';
+import logoInforImg from '@/assets/logo_infor.png';
+
 export default function Footer() {
   return (
-    <footer className="bg-opacity-95p-4 mt-auto w-full bg-primary sm:p-6">
-      <div className="mx-auto w-full p-4 md:max-w-screen-2xl">
+    <footer className="relative mt-auto w-full bg-primary bg-opacity-95 px-5 py-60 sm:px-6 sm:py-12">
+      <div className="mx-auto w-full p-5 md:max-w-screen-2xl">
         <div className="md:flex md:justify-between">
-          <div className="flex w-full flex-col justify-between gap-8 text-white sm:gap-6 md:flex-row md:flex-wrap md:gap-10">
-            <div className="store-data-logo flex flex-col gap-8 text-sm"></div>
+          <div className="-translate-y-1/6 left-100 absolute top-2 transform">
+            <Image
+              className="w-20 rounded-lg transition-all duration-300 hover:scale-105"
+              alt="logoInfor"
+              src={logoInforImg}
+              width={3000}
+              height={1000}
+            />
+          </div>
+
+          {/* Contenido principal con padding superior para evitar solapamiento */}
+          <div className="w-full pt-16 md:pt-20">
+            {' '}
+            <div className="flex flex-col justify-between gap-8 text-white sm:gap-6 md:flex-row md:flex-wrap md:gap-10"></div>
           </div>
         </div>
       </div>
