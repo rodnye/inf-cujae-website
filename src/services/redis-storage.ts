800e50619fa569a859d61e58578111efbf18fb48
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-05-18 15:47:55 -0400

    feat: improvements in Redis session

--- src/services/redis-storage.ts
+++ src/services/redis-storage.ts
@@ -1,20 +1,28 @@
 import Redis from 'ioredis';
 
-export const connectToRedis = () => {
-  const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: Number(process.env.REDIS_PORT) || 6379,
-    username: process.env.REDIS_USER || undefined,
-    password: process.env.REDIS_PASS || undefined,
-  });
+let redis: Redis;
 
-  redis.on('connect', () => {
-    console.log('✅ Conectado a Redis correctamente');
-  });
+export const connectRedis = async () => {
+  if (!redis || redis.status == 'end' || redis.status == 'close') {
+    redis = new Redis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: Number(process.env.REDIS_PORT) || 6379,
+      username: process.env.REDIS_USER || undefined,
+      password: process.env.REDIS_PASS || undefined,
+    });
 
-  redis.on('error', (err) => {
-    console.error('❌ Error de conexión a Redis:', err);
-  });
+    return new Promise<Redis>((resolve, reject) => {
+      redis.on('ready', () => resolve(redis));
+      redis.on('error', (err) => {
+        console.error('❌ Error de conexión a Redis:', err);
+        reject();
+      });
+    });
+  }
 
   return redis;
+};
+
+export const disconnectRedis = () => {
+  redis!.disconnect();
 };
